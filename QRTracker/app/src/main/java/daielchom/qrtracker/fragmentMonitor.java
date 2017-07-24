18779b9a0239ab82041cdbb54d3ad9f6037b8487
DaielChom <danielcarvajalpatino@gmail.com>
2017-07-24 15:42:03 -0500

    GPS [dont work]

--- QRTracker/app/src/main/java/daielchom/qrtracker/fragmentMonitor.java
+++ QRTracker/app/src/main/java/daielchom/qrtracker/fragmentMonitor.java
@@ -1,14 +1,11 @@
 package daielchom.qrtracker;
 
-
 import android.Manifest;
-import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Criteria;
 import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -20,6 +17,10 @@ import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -38,8 +39,9 @@ import static android.content.Context.LOCATION_SERVICE;
  * Created by daielchom on 8/07/17.
  */
 
-public class fragmentMonitor extends Fragment {
+public class fragmentMonitor extends Fragment implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
+    private static final int PETICION_PERMISO_LOCALIZACION = 0;
     private TextView txIDPaquete;
     private TextView txIDOfficial;
     private TextView txHour;
@@ -49,8 +51,8 @@ public class fragmentMonitor extends Fragment {
     private TextView txAlt;
     private Spinner cbState;
     private Button btSend;
-    private LocationManager locManager;
-    private Location loc;
+    GoogleApiClient apiClient;
+
 
     private View view;
 
@@ -74,7 +76,6 @@ public class fragmentMonitor extends Fragment {
         btSend = (Button) view.findViewById(R.id.bt_monitor);
 
 
-
         // Data
         String id_official = getArguments().getString("official");
         String id_paquete = getArguments().getString("id_paquete");
@@ -86,9 +87,17 @@ public class fragmentMonitor extends Fragment {
         String date_monitor = formatter.format(today);
         String hour_monitor = formatterhour.format(today);
 
-        String long_monitor;
-        String alt_monitor;
-        final String lat_monitor;
+        //Data GPS
+        apiClient = new GoogleApiClient.Builder(getActivity()).enableAutoManage(getActivity(), this).addConnectionCallbacks(this).addApi(LocationServices.API).build();
+
+
+        //.enableAutoManage(getActivity(),)
+        //.addConnectionCallbacks(getActivity())
+        //.addApi(Drive.API)
+        //.build();
+
+        //updateUI(loc);
+
 
         //Data state
         final List<String> state = new ArrayList<String>();
@@ -100,7 +109,7 @@ public class fragmentMonitor extends Fragment {
         ArrayAdapter<String> state_monitor = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, state);
 
 
-        if(id_paquete!=null) {
+        if (id_paquete != null) {
             txIDPaquete.setText(id_paquete);
             txIDOfficial.setText(id_official);
             txDate.setText(date_monitor);
@@ -113,14 +122,13 @@ public class fragmentMonitor extends Fragment {
             call.enqueue(new Callback<paquete>() {
                 @Override
                 public void onResponse(Call<paquete> call, Response<paquete> response) {
-                    if(response.body().getId_package() != null){
+                    if (response.body().getId_package() != null) {
 
-                    cbState.setSelection(state.indexOf(response.body().getEstate_package()));
+                        cbState.setSelection(state.indexOf(response.body().getEstate_package()));
 
-                    }else{
-                        Toast.makeText(getContext(),"Paquete no existe", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getContext(), "Paquete no existe", Toast.LENGTH_SHORT).show();
                     }
-
 
 
                 }
@@ -128,14 +136,12 @@ public class fragmentMonitor extends Fragment {
                 @Override
                 public void onFailure(Call<paquete> call, Throwable t) {
 
-                    Toast.makeText(getContext(),"Error ", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getContext(), "Error ", Toast.LENGTH_SHORT).show();
                 }
             });
 
 
-
-        }
-        else {
+        } else {
 
             txIDOfficial.setText(id_official);
             txIDPaquete.setText("READ QR");
@@ -149,14 +155,13 @@ public class fragmentMonitor extends Fragment {
         btSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(txIDPaquete.getText().toString().equals("READ QR")){
+                if (txIDPaquete.getText().toString().equals("READ QR")) {
                     Toast.makeText(getContext(), "Por favor realice la lectira del QR", Toast.LENGTH_SHORT).show();
-                }
-                else {
+                } else {
 
                     QRTrackerService apiServices = retrofitClient.getClient().create(QRTrackerService.class);
 
-                    String id_monitor = txIDPaquete.getText().toString()+txIDOfficial.getText().toString()+txHour.getText().toString()+txDate.getText().toString();
+                    String id_monitor = txIDPaquete.getText().toString() + txIDOfficial.getText().toString() + txHour.getText().toString() + txDate.getText().toString();
                     String id_package = txIDPaquete.getText().toString();
                     String official = txIDOfficial.getText().toString();
                     String lati_monitor = txLat.getText().toString();
@@ -165,8 +170,8 @@ public class fragmentMonitor extends Fragment {
                     String hour_monitor = txHour.getText().toString();
                     String date_monitor = txDate.getText().toString();
 
-                    Monitor monito = new Monitor(id_monitor, id_package, official,  long_monitor,lati_monitor, alti_monitor, hour_monitor, date_monitor);
-                    
+                    Monitor monito = new Monitor(id_monitor, id_package, official, long_monitor, lati_monitor, alti_monitor, hour_monitor, date_monitor);
+
                     Call<Monitor> call = apiServices.postMonitor(monito);
                     call.enqueue(new Callback<Monitor>() {
                         @Override
@@ -192,8 +197,6 @@ public class fragmentMonitor extends Fragment {
                 }
 
 
-
-
             }
         });
 
@@ -202,7 +205,61 @@ public class fragmentMonitor extends Fragment {
 
     }
 
+    private void updateUI(Location loc) {
+        if (loc != null) {
+            txLat.setText(String.valueOf(loc.getLatitude()));
+            txLong.setText(String.valueOf(loc.getLongitude()));
+            txAlt.setText(String.valueOf(loc.getAltitude()));
+        } else {
+            txLat.setText("Desconocida");
+            txLong.setText("Desconocida");
+            txAlt.setText("Desconocida");
+        }
+    }
 
 
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PETICION_PERMISO_LOCALIZACION);
+        }
+        else{
+              Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(apiClient);
+          updateUI(lastLocation);}
 
-}
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.e("LOGTAG", "Error grave al conectar con Google Play Services");
+    }
+
+
+       @Override
+       public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+           if (requestCode == PETICION_PERMISO_LOCALIZACION) {
+               if (grantResults.length == 1
+                   && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                   //Permiso concedido
+
+                   @SuppressWarnings("MissingPermission")
+                   Location lastLocation =
+                       LocationServices.FusedLocationApi.getLastLocation(apiClient);
+
+                   updateUI(lastLocation);
+
+               } else {
+                   //Permiso denegado:
+                   //Deberíamos deshabilitar toda la funcionalidad relativa a la localización.
+
+                   Log.e("LOGTAG", "Permiso denegado");
+               }}}     }
